b262d4b19dec28539c3eb05934a19ecbdca21010
= <63397031+Lucvds99@users.noreply.github.com>
2023-11-15 13:20:26 +0100

    not smooth enough

---
+++ StringAnimator/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static char[] alphabetArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:'\",.<>/? ".toCharArray();
+    public static void main(String[] args) throws InterruptedException {
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            String enterCap = scanner.nextLine();
+
+            System.out.println("enter string: ");
+            String sentence = "this is a test string to stop constently typing it";//scanner.nextLine();
+            boolean finished = false;
+
+            while (!finished){
+                StringBuilder finalSentence = new StringBuilder();
+                char[] array = sentence.toCharArray();
+                for (char c : array) {
+                    for (char alpha : alphabetArray) {
+                        System.out.println(finalSentence.toString() +alpha);
+                        Thread.sleep(0, 20);
+                        if (alpha == c){
+                            finalSentence.append(alpha);
+                            break;
+                        }
+                    }
+                }
+                finished = true;
+            }
+        }
+    }
+}
